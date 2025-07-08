28379f6c4d6da579fc7cccbd5014eef658f9bec3
klaudiaRajs <klaudia.rajs@gmail.com>
2025-07-08 21:26:29 +0200

    Added calculating kcals and GL for a product that is to be added to a recipe.

---
+++ Diabetic/wwwroot/js/utils.js
@@ -0,0 +1,9 @@
+﻿function getKcalsForAmount(amount, kcalPer100g)
+{
+    return Math.round((amount / 100) * kcalPer100g); 
+}
+
+function getGlycemicIndexForAmount(amount, productGI, carbsPer100g)
+{
+    return Math.floor((productGI * (carbsPer100g * (amount / 100))) / 100);
+}
