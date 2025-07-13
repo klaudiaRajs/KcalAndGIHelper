25323365afe763f275742ac156f6f8df580bedf8
klaudiaRajs <klaudia.rajs@gmail.com>
2025-07-13 16:17:54 +0200

    Fixed styling and calculating of the total day kcal and gl.

---
+++ Diabetic/wwwroot/js/dietDay/dietDay.js
@@ -0,0 +1,30 @@
+﻿function updateTotal(recipeId, meal, url) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: {
+            recipeId: recipeId
+        },
+        success: function (res) {
+            var idKcals = "#" + meal + "Kcals";
+            var idGls = "#" + meal + "GLs";
+            $(idKcals)[0].value = res.kcals
+            $(idGls)[0].value = res.gl
+            updateSum(".kcals", "#totalKcals", " kcal, ");
+            updateSum(".gls", "#totalGLs");
+        }
+    });
+}
+
+function updateSum(fieldToUpdate, result, restOfText) {
+    var items = $(fieldToUpdate);
+    var total = 0;
+    for (var i = 0; i < items.length; i++) {
+        var kcal = parseInt(items[i].value);
+        var isANumber = isNaN(kcal) === false;
+        if (kcal != 0 && isANumber) {
+            total += kcal;
+        }
+    }
+    ($(result)[0]).innerHTML = total + (restOfText ? restOfText : "");
+}
